c12a1373f487962159ad7a8fb6b83f2bfd9daeb6
human7905 <daneshlou05@gmail.com>
2026-08-07 17:03:38 +0800

    feat: integrate @vercel/speed-insights/react into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { seedDemoXray } from './services/demoImageSeed';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 // Seed demo X-ray image into IndexedDB on first load (non-blocking)
 seedDemoXray();
@@ -10,5 +11,6 @@ seedDemoXray();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
